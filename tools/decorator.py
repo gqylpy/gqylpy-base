0b1515dac773e8456b6eb273a0ff6b6e8b10567a
Yongkang <zhuyk4@lenovo.com>
2020-07-29 18:39:05 +0800

    'update'

--- tools/decorator.py
+++ tools/decorator.py
@@ -9,28 +9,28 @@ class TryExcept:
 
     def __init__(
             self,
-            except_type: type = Exception,
+            exc_type: type = Exception,
             mark: str = None,
             no_log: bool = False,
-            except_return: ... = None
+            exc_return: ... = None
     ):
         self.mark = mark
         self.no_log = no_log
-        self.except_type = except_type
-        self.except_return = except_return
+        self.exc_type = exc_type
+        self.exc_return = exc_return
 
     def __call__(self, func):
         @functools.wraps(func)
         def inner(*a, **kw):
             try:
                 return func(*a, **kw)
-            except self.except_type as e:
+            except self.exc_type as e:
                 if not self.no_log:
                     sign: str = self.mark or tools.hump(func.__name__)
-                    name: str = type(e).__name__
-                    log.logger.error(f'{sign}.{name}: {e}')
+                    exc_name: str = type(e).__name__
+                    log.logger.error(f'{sign}.{exc_name}: {e}')
 
-            return self.except_return
+            return self.exc_return
 
         return inner
 
@@ -48,23 +48,62 @@ class WhileTrue:
         self.before = before
 
     def __call__(self, func):
-        self.func = func
-        return self.inner
+        @functools.wraps(func)
+        def inner(*a, **kw):
+            while self.cond:
+                if self.before:
+                    time.sleep(self.cycle)
 
-    def inner(self, *a, **kw):
-        while self.cond:
-            if self.before:
-                time.sleep(self.cycle)
+                ret = func(*a, **kw)
 
-            ret = self.func(*a, **kw)
+                if ret == '_break_':
+                    break
+                if ret == '_continue_':
+                    continue
 
-            if ret == '_break_':
-                break
-            if ret == '_continue_':
-                continue
+                if not self.before:
+                    time.sleep(self.cycle)
 
-            if not self.before:
-                time.sleep(self.cycle)
+        return inner
+
+
+class Retry:
+
+    def __init__(
+            self,
+            retry_exc: type = Exception,
+            retry_count: int = None,
+            cycle: int = 10,
+            mark: str = None
+    ):
+        self.retry_exc = retry_exc
+        self.retry_count = retry_count
+        self.mark = mark
+        self.cycle = cycle
+        self.count = 0
+
+    def __call__(self, func):
+        sign: str = self.mark or tools.hump(func.__name__)
+
+        @while_true(cycle=self.cycle)
+        @functools.wraps(func)
+        def wrapper(*a, **kw):
+            try:
+                func(*a, **kw)
+            except self.retry_exc as e:
+                self.count += 1
+                exc_name: str = type(e).__name__
+                msg = f'[retry:{self.count}] {sign}.{exc_name}: {e}'
+
+                if self.retry_count and self.count < self.retry_count:
+                    log.logger.warning(msg)
+                    return
+
+                log.logger.error(msg)
+
+            return '_break_'
+
+        return wrapper
 
 
 def insure(mark: str = None, cycle: int = 10):
