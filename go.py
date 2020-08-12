377fdc3ed7daeff17c1b22c966230b59b9f29713
Yongkang <zhuyk4@lenovo.com>
2020-08-12 12:19:16 +0800

    'update'

--- go.py
+++ go.py
@@ -1,3 +1,12 @@
+import os
+import sys
+
+basedir: str = os.path.dirname(os.path.abspath(__file__))
+
+if basedir not in sys.path:
+    sys.path.insert(0, basedir)
+
+
 import core
 
 if __name__ == '__main__':
